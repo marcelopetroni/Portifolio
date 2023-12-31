18e05724283eef042d5586e8ecb794a64389fdce
marcelopetroni <marceloapetroni@hotmail.com>
2023-12-30 21:02:11 -0300

    Primeiros passos

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "../styles/header.sass";
+import { TfiLinkedin } from "react-icons/tfi";
+import { SiGmail } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+
+const header = () => {
+  return (
+    <header className='header-container'>
+      <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
+        <div className="tres-barras-container">
+          <div className="barra"></div>
+          <div className="barra"></div>
+          <div className="barra"></div>
+        </div>
+
+        <div className="nav-container">
+            <a>Sobre</a>
+            <a>Projetos</a>
+            <a>Skills</a>
+            <a>Contatos</a>
+        </div>
+        <div className="social-icons-container">
+            <button className='linkedin-button'>
+              <TfiLinkedin />
+            </button>
+
+            <button className='gmail-button'>
+              <SiGmail/>
+            </button>
+           
+            <button className='github-button'>
+              <FaGithub />
+            </button>
+        </div>
+    </header>
+  )
+}
+
+export default header
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/home.sass";
+
+const main = () => {
+  return (
+    <main>
+      <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
+      <div className="titulo-container">
+        <h1 className='titulo'>Olá, meu nome é</h1>
+        <h1 className='titulo2'>Marcelo Petroni</h1>
+      </div>
+    </main>
+  )
+}
+
+export default main
