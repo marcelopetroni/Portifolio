d6f284b52b8adf39825979cb77c62c8e0563371c
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-01 22:39:03 -0300

    elementos iniciais

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,19 @@ const main = () => {
   return (
     <main>
       <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet"></link>
+      
       <div className="titulo-container">
         <h1 className='titulo'>Olá, meu nome é</h1>
         <h1 className='titulo2'>Marcelo Petroni</h1>
+        <h3 className='descricao'>Transformo linhas de código em inovação.</h3>
+        <p className='descricao2'>Sou estudante de Ciência da computação, e também,</p>
+        <p className='descricao2'>sou aspirante em Desenvolvimento Front-end ツ</p>
+      </div>
+      <div className="icon-container">
+        <h1 className='sombra'>M</h1>
+        <h1 className='frontal'>M</h1>
       </div>
     </main>
   )
