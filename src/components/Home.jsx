8813e9d89173a97635cf1fddfad4b5e0854033ac
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-12 18:15:18 -0300

    ajueste de espaçamento e valores

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,24 +13,28 @@ const main = () => {
       <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3&display=swap" rel="stylesheet"></link>
       <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet"></link>
       
-      <div className="titulo-container">
-        <h1 className='titulo'>Olá, meu nome é</h1>
-        <h1 className='titulo2'>Marcelo Petroni</h1>
-        <h3 className='descricao'>Transformo linhas de código em interfaces.</h3>
-        <p className='descricao2'>Estudante de Ciência da computação, e também,</p>
-        <p className='descricao2'>sou aspirante em Desenvolvimento Front-end ツ</p>
-      </div>
-      <div className="icon-container">
-        <h1 className='sombra'>M</h1>
-        <h1 className='frontal'>M</h1>
-      </div>
-      <h2>Projetos</h2>
-      <div className="cards-container">
-        <Card content = {<Crud/>}/>
-        <Card content = {<TodoList/>}/>
-        <Card content = {<BuscadorDecCep/>}/>
-        <Card content = {<Landingpage/>}/>
-      </div>
+      <section className='about-container'>
+        <div className="titulo-container">
+          <h1 className='titulo'>Olá, meu nome é</h1>
+          <h1 className='titulo2'>Marcelo Petroni</h1>
+          <h3 className='descricao'>Transformo linhas de código em interfaces.</h3>
+          <p className='descricao2'>Estudante de Ciência da computação, e também,</p>
+          <p className='descricao2'>sou aspirante em Desenvolvimento Front-end ツ</p>
+        </div>
+        <div className="icon-container">
+          <h1 className='sombra'>M</h1>
+          <h1 className='frontal'>M</h1>
+        </div>
+      </section>
+      <h2 className='projects-title'>Projetos</h2>
+      <section className='projects-container'>
+        <div className="cards-container">
+          <Card content = {<Crud/>}/>
+          <Card content = {<TodoList/>}/>
+          <Card content = {<BuscadorDecCep/>}/>
+          <Card content = {<Landingpage/>}/>
+        </div>
+      </section>    
     </main>
   )
 }
