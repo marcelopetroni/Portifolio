0ae2d40444eee3830cd5efd96e8b6abff7e0d8d9
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-20 19:57:16 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ViteSassPlugin from 'vite-plugin-sass';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSassPlugin(),
-  ],
+  plugins: [react()], 
 })
