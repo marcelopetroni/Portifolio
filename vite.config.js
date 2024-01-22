fbc203cbdb6a56f5438cbcff9f7cc7da0d8e57cd
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-21 22:22:26 -0300

    configuração

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sass from 'vite-plugin-sass';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sass()], 
-  build: {
-    rollupOptions: {
-      external: ['/src/styles/footer.sass', "/src/styles/card.sass", "/src/styles/home.sass", "/src/styles/header.sass"], 
-    },
-  },
+  plugins: [react()], 
 })
