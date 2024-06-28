0d241f299e0600ff2e12867a91f9a73b557fd48c
marcelopetroni <marceloapetroni@hotmail.com>
2024-06-27 23:33:38 -0300

    correção estética

--- src/projects/landingPage/index.jsx
+++ src/projects/landingPage/index.jsx
@@ -9,7 +9,7 @@ const Landingpage = () => {
       <div className="img">
         <img src={background} alt="Landing Page project image" className='imagem'/>
       </div>
-      <p className="job">Landing page project made in HTML CSS and incorporated into React.js. The aim was to learn how to create a fixed header in a page.</p>
+      <p className="job">Landing page project made in HTML CSS and incorporated into React.js. Learned how to create a fixed header in a page.</p>
       <div className="buttons">
         <button><a href="https://github.com/marcelopetroni/LandingPage" target='_blank'>Repository</a></button>
         <button><a href="https://landing-page-henna-one.vercel.app/" target='_blank'>Deploy</a></button>
